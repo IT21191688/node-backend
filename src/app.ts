338f470484c92622c348034c86300eebfdc31050
Thanuja Wickramasinghe <99541354+thanujaD00@users.noreply.github.com>
2025-03-13 00:38:37 +0530

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -35,10 +35,7 @@ class App {
 
         this.app.set('trust proxy', 'loopback, linklocal, uniquelocal');
 
-        // Security middleware
-        this.app.use(helmet({
-            contentSecurityPolicy: process.env.NODE_ENV === 'production' ? undefined : false
-        }));
+
         
         this.app.use(cors({
             origin: config.corsOrigin,
