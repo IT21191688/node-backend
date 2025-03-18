999dbdb3517bbf4d600552b03781b5dd82107f53
it21096570 <it21096570@my.sliit.lk>
2025-03-18 22:52:56 +0530

    soil m

--- src/services/YieldPredictionService.ts
+++ src/services/YieldPredictionService.ts
@@ -1,5 +1,9 @@
 import axios from 'axios';
 import YieldPrediction from '../models/YieldPrediction';
+import logger from '../utils/logger';
+import { WateringService } from '../services/wateringService';
+import { Location } from "../models/location";
+
 
 class YieldPredictionService {
   private predictionApiUrl = 'https://flask-be-deploy.onrender.com/predict';
@@ -22,15 +26,50 @@ class YieldPredictionService {
   }
 
   public async createYieldPrediction(data: any, userId: string, locationId: string): Promise<any> {
-    const predictionResponse = await this.predictYield(data);
-    const yieldPrediction = new YieldPrediction({
-      ...predictionResponse,
-      user: userId,
-      location: locationId,
-    });
-    await yieldPrediction.save();
-    return yieldPrediction;
+    try {
+      // Create an instance of WateringService and fetch soil moisture data
+      const location = await Location.findOne({ _id: locationId});
+      const wateringService = new WateringService();
+      
+      if (!location || !location.deviceId) {
+        throw new Error('Location not found or device ID not available');
+      }
+      
+      let moistureSensorData = await wateringService.getSoilMoistureData(location.deviceId);
+
+      // Log the retrieved moisture sensor data
+      console.log("Moisture sensor data:", moistureSensorData);
+
+      // Update soil moisture values if moistureSensorData is available
+      if (moistureSensorData && data.monthly_data && data.monthly_data.length > 0) {
+        data.monthly_data[0].sm_10 = moistureSensorData.moisture10cm;
+        data.monthly_data[0].sm_20 = moistureSensorData.moisture20cm;
+        data.monthly_data[0].sm_30 = moistureSensorData.moisture30cm;
+      }
+
+      // Add moisture sensor data to the prediction data
+      const predictionData = { ...data };
+
+      // Log the complete prediction data
+      console.log("Predict yield data:", data);
+
+      const predictionResponse = await this.predictYield(predictionData);
+      const yieldPrediction = new YieldPrediction({
+        ...predictionResponse,
+        user: userId,
+        location: locationId,
+      });
+      await yieldPrediction.save();
+      return yieldPrediction;
+    } catch (error) {
+      if (error instanceof Error) {
+        throw new Error('Error creating yield prediction: ' + error.message);
+      } else {
+        throw new Error('Error creating yield prediction');
+      }
+    }
   }
+
   public async getAllYieldPredictions(): Promise<any> {
     return YieldPrediction.find();
   }
